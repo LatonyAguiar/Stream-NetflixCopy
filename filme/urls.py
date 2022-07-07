4d5b59514c4ffa60aba37984a9e430e4d3c053f1
Latony <latonyaguiar@gmail.com>
2022-07-07 12:00:09 -0300

    Criação de links dinamicos

--- filme/urls.py
+++ filme/urls.py
@@ -3,9 +3,10 @@
 from django.urls import path, include
 from .views import Homepage, Homefilmes, Detalhesfilme
 
+app_name = 'filme'
 
 urlpatterns = [
-    path('', Homepage.as_view()),
-    path('filmes/', Homefilmes.as_view()),
-    path('filme/<int:pk>', Detalhesfilme.as_view()),
+    path('', Homepage.as_view(), name='homepage'),
+    path('filmes/', Homefilmes.as_view(), name='filme'),
+    path('filme/<int:pk>', Detalhesfilme.as_view(), name='detalhesfilme'),
 ]
